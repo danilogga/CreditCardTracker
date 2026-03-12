679adc25eefef7d9ce259980f6ab91be80675c7c
Danilo Carvalho <danilo@flexdoc.com.br>
2026-03-12 19:13:02 -0400

    Initial Commit

---
+++ CreditCardTracker/CreditCardTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardTrackerApp.swift
+//  CreditCardTracker
+//
+//  Created by Danilo Carvalho on 12/03/26.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
